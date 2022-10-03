90f9ce287b0ab26b1c1cbad70a99878d8c92cff0
jamesdosstech <jamesdosstech@gmail.com>
2022-10-03 15:09:39 -0500

    implement context for shopping bag product card and shop and implemented a cart dropdown with styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,13 +2,20 @@ import { Outlet, Link } from 'react-router-dom'
 import { Fragment, useContext } from 'react';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from '../../context/user.context';
+import { CartContext } from '../../context/cart.context';
+
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
       <Fragment>
@@ -32,7 +39,11 @@ const Navigation = () => {
                 </Link>
               )
             }
+            <CartIcon />
           </div>
+          { 
+            isCartOpen && <CartDropdown /> 
+          }
         </div>
         <Outlet />
       </Fragment>
