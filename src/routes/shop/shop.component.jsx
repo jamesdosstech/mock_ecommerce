e32edd727002331d9e74245fe2c85ccb8b7011e5
jamesdosstech <jamesdosstech@gmail.com>
2022-12-06 16:48:15 -0600

    added categories saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import './shop.styles.scss'
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { fetchCategoriesAsync } from '../../store/categories/categories.action';
+import { fetchCategoriesStart } from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component'
@@ -11,7 +11,7 @@ import { useEffect } from 'react';
 const Shop = () => {
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(fetchCategoriesAsync());
+        dispatch(fetchCategoriesStart());
     },[dispatch])
     return (
         <Routes>
