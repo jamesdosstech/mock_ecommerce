aaa6a99464ffb132e55e1e62f3eb8eeb01cf8671
jamesdosstech <jamesdosstech@gmail.com>
2023-03-23 16:00:56 -0500

    converted categories reducer to typescript

--- src/store/categories/categories.types.ts
+++ src/store/categories/categories.types.ts
@@ -1,9 +1,25 @@
-const CATEGORIES_ACTION_TYPES = {
+enum CATEGORIES_ACTION_TYPES {
     // SET_CATEGORIES: 'category/SET_CATEGORIES',
-    FETCH_CATEGORIES_START: 'FETCH_CATEGORIES_START',
-    FETCH_CATEGORIES_SUCCESS: 'FETCH_CATEGORIES_SUCCESS',
-    FETCH_CATEGORIES_FAILED: 'FETCH_CATEGORIES_FAILED',
+    FETCH_CATEGORIES_START = 'FETCH_CATEGORIES_START',
+    FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS',
+    FETCH_CATEGORIES_FAILED = 'FETCH_CATEGORIES_FAILED',
 };
 
 export default CATEGORIES_ACTION_TYPES
-  
+
+export type CategoryItem = {
+    id: number;
+    imageUrl: string;
+    name: string;
+    price: number;
+}
+
+export type Category = {
+    title: string;
+    imageUrl: string;
+    items: CategoryItem[]
+}
+
+export type CategoryMap = {
+    [key: string]: CategoryItem[];
+}
